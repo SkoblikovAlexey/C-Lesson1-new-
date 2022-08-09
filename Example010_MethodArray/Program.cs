25fd25ae77d05165a52dddeb8477a08c03c42ccb
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-09 15:25:02 +0300

    Example 010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 23, 454, 98, 43, 23, 7, 56, 90 };
+
+int n = array.Length;
+int find = 23;
+
+int index = 0;
+
+while (index < n) {
+    if (array[index] == find) {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
