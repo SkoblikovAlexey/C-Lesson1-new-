25a2d0ca5da22e324f54fe2ca900949db4484c26
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-31 04:17:17 +0300

    Example 001 final

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Получение информации о директориях и файлах путем перебора
+
+void CatalogInfo(string path, string indent = "")    // string indent = "" здесь это отступ, чтобы видеть структуру папки
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + "  ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+string path = "D:/GeekBrains/C#/Lections/C-Lesson1-new-/Example001_HelloConsole";
+CatalogInfo(path);
